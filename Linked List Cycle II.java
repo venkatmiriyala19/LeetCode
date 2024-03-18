5b51dd0118d232f19d73fe6ab74e4446a52a2645
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-03-18 21:42:40 +0530

    Create Linked List Cycle II.java

---
+++ Linked List Cycle II.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if (slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
