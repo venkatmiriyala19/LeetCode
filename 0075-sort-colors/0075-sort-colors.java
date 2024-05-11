e3422f285685d9535bbbe30e6da0d8f5613e1089
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-11 13:55:11 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (10.43%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if ((nums[i]))
+        // }
+
+         Arrays.sort(nums);
+    }
+}
