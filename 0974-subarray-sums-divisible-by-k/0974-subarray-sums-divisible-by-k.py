90264d1f3356e8d75da70e2a2eb94576d02ba737
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-06-09 09:50:56 +0530

    Time: 215 ms (85.37%), Space: 15 MB (61.79%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+        count=0
+        prefix_sum=0
+        prefix_map={0:1}
+        for num in nums:
+            prefix_sum+=num
+            mod=prefix_sum%k
+            if mod<0:
+                mod+=k
+            if mod in prefix_map:
+                count+=prefix_map[mod]
+                prefix_map[mod]+=1
+            else:
+                prefix_map[mod]=1
+        return count
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
