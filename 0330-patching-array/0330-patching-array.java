c8cf9eb73f02e80b62999f0419a14834a9739999
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-06-16 11:11:30 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (95.47%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss=1;
+        int result=0,i=0;
+        while(miss<=n){
+            if (i<nums.length && nums[i]<=miss){
+                miss+=nums[i];
+                i++;
+            }
+            else{
+                miss+=miss;
+                result++;
+            }
+        }
+        return result;
+        
+    }
+}
