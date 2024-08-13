bb472d4936957a62289c224d6c3bb53df16863b0
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-08-13 16:05:47 +0530

    Time: 49 ms (81.30%), Space: 16.7 MB (30.23%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        ans = []
+        ds = []
+        candidates.sort()
+
+
+        def findCombination(ind, target):
+            if target == 0:
+                ans.append(ds[:])
+                return
+            for i in range(ind, len(candidates)):
+                if i > ind and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                ds.append(candidates[i])
+                findCombination(i + 1, target - candidates[i])
+                ds.pop()
+
+
+        findCombination(0, target)
+        return ans
+        
